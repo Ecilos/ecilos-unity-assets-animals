46d91a530b88f505a8345b277a89465e7ac7d78c
kenorb <kenorb@users.noreply.github.com>
2023-02-14 01:44:34 +0000

    Reformats scripts with clang

--- Assets/Birds/Scripts/lb_CrowProximity.cs
+++ Assets/Birds/Scripts/lb_CrowProximity.cs
@@ -2,11 +2,10 @@
 using System.Collections;
 
 public class lb_CrowProximity : MonoBehaviour {
-	
-	void OnTriggerEnter (Collider col) {
-		if(col.tag == "lb_bird"){
-			col.SendMessage("CrowIsClose");
-		}
-	}
 
+  void OnTriggerEnter(Collider col) {
+    if (col.tag == "lb_bird") {
+      col.SendMessage("CrowIsClose");
+    }
+  }
 }
